b797ee4fc8ac0050a09ec510ccc3065620181e9a
e278773 <e278773@UK07DT7QY6NZ1.global.ds.honeywell.com>
2016-11-04 14:49:57 +0000

    Update

---
+++ src/com/honeywell/relay/FaultType.java
@@ -0,0 +1,22 @@
+package com.honeywell.relay;
+
+public class FaultType
+{
+	//	Zone type variable
+	public static final String ACPOWER = "011";
+	public static final String AUXTAMPER = "012";
+	public static final String LIDTAMPER = "013";
+	public static final String WALLTAMPER = "014";
+	public static final String LINEFAIL = "015";
+	public static final String COMMFAIL = "016";
+	public static final String BATTFAIL1 = "017";
+	public static final String BATTFAIL2 = "018";	
+	public static final String FUSE1 = "021";
+	public static final String FUSE2 = "022";
+	public static final String FUSE3 = "023";
+	public static final String SPARE1 = "024";
+	public static final String SPARE2 = "025";
+	public static final String SPARE3 = "026";
+	public static final String SPARE4 = "027";
+	public static final String SPARE5 = "028";
+}
---
+++ src/com/honeywell/relay/RelayBoard.java
@@ -0,0 +1,171 @@
+package com.honeywell.relay;
+
+import jssc.*;
+
+/**
+* The RelayBoard application allows control of the KMTronic relay boards for zone/fault 
+* creation on test panels.
+*
+* @author  Tony Crichton
+* @version 1.1.0
+* @since   30-10-2016
+*/
+
+public class RelayBoard
+{
+	SerialPort serialPort;
+	String com;
+	String receivedData;
+	int rioNo;
+	int zoneNo;
+	int relay;
+	int Offset;
+		
+	/**
+	   * This constructor is used to create serial connection to USB controller. 
+	   * 
+	   * @param comPort This is the serial COM port to connect.
+	   */
+	
+	public RelayBoard(String comPort) throws SerialPortException
+	{
+		
+			com = comPort;	
+			serialPort = new SerialPort(com);
+	}
+	
+	 /**
+	   * This method is used to create panel system faults. 
+	   * 
+	   * @param failure This is the type of failure to be created
+	   * @param state  This is the zone state to be created
+	   */
+	
+	public String setFault(String failure, int state) throws SerialPortException, InterruptedException 
+	{
+		int data[] = {255, ConvertZone(failure, 0), state, 255, (160+rioNo) - Offset, 0};
+		SerialTx(data, 0);		
+		return receivedData;
+		
+	}
+	
+	/**
+	   * This method is used to create panel system faults with debounce time. 
+	   * 
+	   * @param failure This is the type of failure to be created
+	   * @param state  This is the zone state to be created
+	   * @param debounce This is the programmed debounce time (ms)
+	   */
+	
+	public String setFault(String failure, int state, int debounce) throws SerialPortException, InterruptedException 
+	{
+		int data[] = {255, ConvertZone(failure, 0), state, 255, (160+rioNo) - Offset, 0};
+		SerialTx(data, debounce);
+		return receivedData;
+	}	
+	
+	/**
+	   * This method is used to create panel zone activation with debounce time and addr offset. 
+	   * 
+	   * @param zone This is the zone to be activated
+	   * @param state  This is the zone state to be created
+	   * @param debounce This is the programmed debounce time (ms)
+	   * @param offset This is the RelayBoard addr offset (0 or 1)
+	   */
+	
+	public String setZone(String zone, int state, int debounce, int offset) throws SerialPortException, InterruptedException 
+	{
+		int data[] = {255, ConvertZone(zone, offset), state, 255, (160+rioNo) - Offset, 0};
+		SerialTx(data, debounce);		
+		return receivedData;
+	}	
+	
+	/**
+	   * This method is used to create panel zone activation with debounce time and addr offset. 
+	   * 
+	   * @param zone This is the zone to be activated
+	   * @param state  This is the zone state to be created
+	   * @param offset This is the RelayBoard addr offset (0 or 1)
+	   */
+
+	public String setZone(String zone, int state, int offset) throws SerialPortException, InterruptedException  
+	{
+		int data[] = {255, ConvertZone(zone, offset), state, 255, (160+rioNo) - Offset, 0};
+		SerialTx(data, 0);
+		return receivedData;
+	}	
+	
+		
+	
+	private void SerialTx(int[] data, int debounce) throws SerialPortException, InterruptedException 
+	{
+		int dataClose[] = {255, data[1], 0};	
+					
+		//serialPort = new SerialPort(com);
+					
+				serialPort.openPort();
+								
+				serialPort.setParams(SerialPort.BAUDRATE_9600,
+                        SerialPort.DATABITS_8,
+                        SerialPort.STOPBITS_1,
+                        SerialPort.PARITY_NONE);
+   		   	   
+				serialPort.addEventListener(new PortReader(), SerialPort.MASK_RXCHAR);
+		   
+		   serialPort.writeIntArray(data);
+		    		    		    
+		    if (debounce > 0)
+		    {
+		    	Thread.sleep(debounce);
+		    	
+		    	serialPort.writeIntArray(dataClose);
+		    }
+		    
+		    Thread.sleep(50);
+		    serialPort.closePort();
+		    		    
+	}
+	
+	private int ConvertZone(String zone, int offset)
+	{
+		Offset = offset;
+		rioNo = Integer.parseInt(zone.substring(0, 2));
+		zoneNo = Integer.parseInt(zone.substring(2, 3));
+		relay = (rioNo-(1 + offset)) * 8 + zoneNo;
+		return relay;
+		
+	}
+	
+	
+	private class PortReader implements SerialPortEventListener 
+	{
+
+	    @Override
+	    public void serialEvent(SerialPortEvent event) 
+	    {
+	    		    	
+	        if(event.isRXCHAR() && event.getEventValue() > 9 && event.getEventValue() <= 10) 
+	        {
+	            try
+	            {
+	            	receivedData = serialPort.readHexString(event.getEventValue());
+	                
+	            }
+	            catch (SerialPortException ex) 
+	            {
+	                receivedData = "Error in receiving string from COM-port: " + ex;
+	            }
+	        }
+	        
+	       
+	   }
+	            
+	  }
+}	
+	
+
+
+
+
+
+
