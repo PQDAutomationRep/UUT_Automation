d59e1de7c2f28bc691c6f3575a7b46a882a85750
e278773 <e278773@UK07DT7QY6NZ1.global.ds.honeywell.com>
2016-11-04 12:41:25 +0000

    Initial Update

---
+++ Automation/src/com/honeywell/relay/State.java
@@ -0,0 +1,9 @@
+package com.honeywell.relay;
+
+public class State 
+{
+	// Relay state variables
+	public static final int TRIP = 1;
+	public static final int OPEN = 1;
+	public static final int CLOSE = 0;
+}
---
+++ Automation/src/com/honeywell/ssh/SSH.java
@@ -0,0 +1,121 @@
+package com.honeywell.ssh;
+
+import java.io.IOException;
+import java.io.InputStream;
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+//import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+//import com.jcraft.jsch.SftpException;
+
+/**
+* Application to run shell commands on SSH enabled panel.
+*
+* @author  Tony Crichton
+* @version 1.0.0
+* @since   31-10-2016
+*/
+
+public class SSH 
+{
+	
+	Session session;
+	int i;
+	
+	/**
+	   * This Constructor is used to establish SSH connection to remote panel. 
+	   * 
+	   * @param host   Remote panel IP
+	   * @param user   user login credential
+	   * @param password password login credential
+	   */
+		
+	public SSH(String host, String user, String password)
+	{
+	try
+		{	    	
+	    	java.util.Properties config = new java.util.Properties(); 
+	    	config.put("StrictHostKeyChecking", "no");
+	    	JSch jsch = new JSch();
+	    	session=jsch.getSession(user, host, 22);
+	    	session.setPassword(password);
+	    	session.setConfig(config);
+	    	session.connect();
+	    	System.out.println("\r SSH Session Connected");
+		}
+	catch(Exception e)
+		{
+		    e.printStackTrace();
+		}
+	}
+	
+	/**
+	   * This method is used to send required shell command to panel over established SSH connection. 
+	   * 
+	   * @param command   Shell command to be sent to remote panel
+	   */
+	
+	public String sendCommand(String command) throws JSchException, IOException
+	{
+		Channel channel=session.openChannel("exec");
+        ((ChannelExec)channel).setCommand(command);
+        System.out.print("\rCommand Sent: " + command + "\r");
+        channel.setInputStream(null);
+        ((ChannelExec)channel).setErrStream(System.err);
+        InputStream in=channel.getInputStream();
+        channel.connect();
+        byte[] tmp=new byte[1024];
+        while(true)
+        {
+          while(in.available()>0)
+          {
+            i=in.read(tmp, 0, 1024);
+            if(i<0)break;
+            System.out.print("Command Reply: " + new String(tmp, 0, i) + "\r");
+          }
+          if(channel.isClosed())
+          {
+        	  
+            System.out.println("exit-status: " + channel.getExitStatus());
+            break;
+          }
+          try{Thread.sleep(1000);}catch(Exception ee){}
+        }
+        channel.disconnect();
+        //session.disconnect();
+        return "Exit Status: " + Integer.toString(channel.getExitStatus());
+		
+	}
+	
+	/*public String sendFile(String sourceFile, String destination)
+	{
+		try
+		{           
+            
+            session.connect(); 
+            ((ChannelExec)channel).setErrStream(System.err);
+            Channel channel = session.openChannel("sftp");
+            channel.connect();
+            ChannelSftp sftpChannel = (ChannelSftp) channel; 
+            sftpChannel.get(sourceFile, destination);
+            sftpChannel.exit();
+            session.disconnect();
+        } 
+		catch (JSchException e)
+		{
+            e.printStackTrace();  
+        } 
+		catch (SftpException e) 
+		{
+            e.printStackTrace();
+        }
+		return "Exit Status: " + Integer.toString(channel.getExitStatus());
+ 
+
+ 
+    }*/
+	
+
+}
