89faced2fd25227c957b8dfe1042afb6a4eefba9
e278773 <e278773@UK07DT7QY6NZ1.global.ds.honeywell.com>
2016-11-08 11:07:58 +0000

    Removed unwanted comments

--- src/com/honeywell/ssh/SSH.java
+++ src/com/honeywell/ssh/SSH.java
@@ -8,7 +8,7 @@ import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
-//import com.jcraft.jsch.SftpException;
+
 
 /**
 * Application to run shell commands on SSH enabled panel.
